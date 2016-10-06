1954be8a9a2ccce15673e46833a812aef07d641c
UjiAhmadFauzi <ahmad.uji1902@gmail.com>
2016-10-06 14:52:09 +0700

    SISMIOP Minahasa First Commit on Oct 6, 2016

---
+++ bin/dbconn.php
@@ -0,0 +1,17 @@
+<?php
+#Oracle Connector
+// $c = oci_connect('pbb', 'pbb', 'localhost/XE');
+
+// #MariaDB Conn
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "pbb";
+//
+// // Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
